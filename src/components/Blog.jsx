7f34cf7ef14d888a03e08135c4bc499705fb9e9f
Iñigo Alonso Fernandez <inigoalonso@inigoalonso.com>
2024-07-08 15:23:36 +0200

    5.7

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -1,7 +1,27 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import React, { useState } from 'react';
 
-export default Blog
+const Blog = ({ blog }) => {
+  const [detailsVisible, setDetailsVisible] = useState(false);
+
+  const toggleDetails = () => {
+    setDetailsVisible(!detailsVisible);
+  }
+
+  return (
+    <div className="blog">
+      <div>
+        {blog.title} {blog.author}
+        <button onClick={toggleDetails}>{detailsVisible ? 'hide' : 'view'}</button>
+      </div>
+      {detailsVisible && (
+        <div className="blog-details">
+          <p>{blog.url}<br />
+          likes {blog.likes} <button>like</button><br />
+          {blog.author}</p>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default Blog;
